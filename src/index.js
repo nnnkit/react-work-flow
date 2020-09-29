10ce1c9823eaf4c7587e641311120afb02ee1d6b
Ankit <iankit17@gmail.com>
2020-09-29 13:28:54 +0530

    upadte message

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 function App(props) {
-  return <h1>Hello App</h1>;
+  return <h1>Hello App Again</h1>;
 }
 
 render(<App />, document.getElementById('root'));
